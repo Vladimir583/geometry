9a134441165b7653dc45ea654ce8681d813f64d3
Vladimir583 <zemfv@Mail.ru>
2019-03-22 14:07:27 +0700

    Add function for reading, record from file

--- geometry.c
+++ geometry.c
@@ -1,17 +1,59 @@
-#include<stdio.h>
-#include<ctype.h>
-int main()
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+#include "geometry.h"
+
+
+#define TRIANGLE 1
+#define CIRCLE 2
+
+
+int Parser(Figure *new, char *A)
 {
-	int x,y;
-	float r,per,plo;
-	printf("Enter (x,y)\n");
-	scanf("%d  %d", &x,&y);
-	printf("Enter radius\n");
-	scanf("%f", &r);
-	per = 2 * 3.14 * r;
-	plo = 3.14 * r * r;
-	printf("Perimeter = %f\n", per);
-	printf("Area = %f\n", plo);
-	return 0;
+  char B[256];
+  int i = 0;
+  while (A[i] >= 'a' && A[i] <= 'z')
+  {
+    B[i] = A[i];
+    i++;
+  }
+  B[i] = '\0';
+  if (!(strcmp(B, "triangle")))
+  {
+    new->type = TRIANGLE;
+  }
+  else if (!(strcmp(B, "circle")))
+  {
+    new->type = CIRCLE;
+  }
+  else
+  {
+    printf("Unknown type\n");
+    return 1;
+  }
+  char *end;
+  end = A;
+  i = -1;
+  new->coordinates = (double *)malloc(sizeof(double) * 100);
+  if (new->coordinates == NULL)
+  {
+    printf("Alloceted error");
+    return 1;
+  }
+  while (*A)
+  {
+    new->coordinates[i] = strtod(A, &end);
+    A = end;
+    i++;
+    while (!(isdigit(*A) || *A == '-' || *A == '+') && *A)
+    {
+      A++;
+    }
+  }
+  new->size = i;
+  return 0;
 }
+
 
